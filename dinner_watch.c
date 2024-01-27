b56261b7635bc85b08d06f7e6753ba0d4623d40b
LegendreMatisse <legendre.matisse@gmail.com>
2024-01-27 23:27:54 +0100

    Update dinner_watch.c

--- dinner_watch.c
+++ dinner_watch.c
@@ -56,6 +56,7 @@ void philo_dead(t_philo *philo)
 {
 	if (philo_died(philo))
 	{
+		printf("baguette\n");
 		write_status(DIED, philo);
 		set_bool(&philo->philo_mutex, &philo->all_meals, true);
 		set_bool(&philo->data->data_mutex, &philo->data->end, true);
